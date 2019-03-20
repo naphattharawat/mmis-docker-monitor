9d9284c9fce54a895895bb8f45e57aa48c482d46
naphattharawat <naphattharawat@gmail.com>
2019-03-20 13:18:30 +0700

    add monirot 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { Monitor2Component } from './monitor2/monitor2.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MonitorComponent } from './monitor/monitor.component';
+import { Monitor2Component } from './monitor2/monitor2.component';
+import { Monitor3Component } from './monitor3/monitor3.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'monitor', pathMatch: 'full' },
   { path: 'monitor', component: MonitorComponent },
-  { path: 'monitor2', component: Monitor2Component }
+  { path: 'monitor2', component: Monitor2Component },
+  { path: 'monitor3', component: Monitor3Component }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { MonitorComponent } from './monitor/monitor.component';
 import { MonitorService } from './monitor.service';
 import { ClarityModule } from '@clr/angular';
 import { Monitor2Component } from './monitor2/monitor2.component';
+import { Monitor3Component } from './monitor3/monitor3.component';
 @NgModule({
   declarations: [
     AppComponent,
     MonitorComponent,
-    Monitor2Component
+    Monitor2Component,
+    Monitor3Component
   ],
   imports: [
     BrowserModule,
